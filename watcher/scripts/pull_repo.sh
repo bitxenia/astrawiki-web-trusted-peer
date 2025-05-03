4f696655339960f45792e9535a6925a36595dc4f
Joaquin Velazquez <jvelazquez@fi.uba.ar>
2025-05-03 11:34:34 -0300

    add more resilient error handling for repo pulling

--- watcher/scripts/pull_repo.sh
+++ watcher/scripts/pull_repo.sh
@@ -20,27 +20,63 @@ cd "${SRC_DIR}" || exit 1
 git config --global url."https://github.com/".insteadOf git@github.com:
 
 if [ ! -d ".git" ]; then
-	if ! git clone --quiet "${REPO_ADDRESS}" --branch "${REPO_BRANCH}" --single-branch .; then
-		echo "Failed to clone git repository" >&2
-		exit 1
-	fi
-	printf "true"
-	exit 0
+	tries=0
+	MAX_TRIES=10
+	readonly MAX_TRIES
+	while :; do
+		if ! git clone --quiet "${REPO_ADDRESS}" --branch "${REPO_BRANCH}" --single-branch .; then
+			tries=$((tries + 1))
+			if [ "${tries}" -eq "${MAX_TRIES}" ]; then
+				echo "Failed to clone git repository" >&2
+				exit 1
+			fi
+			printf "Couldn't clone repository: %s\n" "${REPO_ADDRESS}" >&2
+			echo 'Trying again in 10 seconds...' >&2
+			sleep 10
+		else
+			printf "true"
+			exit 0
+		fi
+	done
 fi
 
-if ! git fetch origin --quiet; then
-	echo "Failed to fetch from repository: ${REPO_ADDRESS}" >&2
-	exit 1
-fi
+tries=0
+MAX_TRIES=10
+readonly MAX_TRIES
+while :; do
+	if ! git fetch origin --quiet; then
+		tries=$((tries + 1))
+		if [ "${tries}" -eq "${MAX_TRIES}" ]; then
+			echo "Failed to fetch from repository: ${REPO_ADDRESS}" >&2
+			exit 1
+		fi
+		printf "Couldn't fetch from repository: %s\n" "${REPO_ADDRESS}" >&2
+		echo "Trying again in 10 seconds..." >&2
+		sleep 10
+	else
+		break
+	fi
+done
 
 if ! git diff --quiet "${REPO_BRANCH}" "origin/${REPO_BRANCH}"; then
-	if ! git pull --quiet; then
-		echo "Failed to pull git repository" >&2
-		exit 1
-	else
-		printf "true"
-		exit 0
-	fi
+	tries=0
+	MAX_TRIES=10
+	readonly MAX_TRIES
+	while :; do
+		if ! git pull --quiet; then
+			tries=$((tries + 1))
+			if [ "${tries}" -eq "${MAX_TRIES}" ]; then
+				echo "Failed to pull git repository" >&2
+				exit 1
+			fi
+			printf "Couldn't pull from repository: %s\n" "${REPO_ADDRESS}" >&2
+			echo "Trying again in 10 seconds..." >&2
+			sleep 10
+		else
+			printf "true"
+			exit 0
+		fi
+	done
 fi
 
 printf "false"
